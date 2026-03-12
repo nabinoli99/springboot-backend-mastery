ededcc055cea142aa07d49029b59cadd10be8aa3
Suckscorpio999 <olinabin214@gmail.com>
2026-03-12 11:02:50 +0545

    feat: updated StudentService interface

--- src/main/java/com/learn/services/StudentService.java
+++ src/main/java/com/learn/services/StudentService.java
@@ -1,14 +1,18 @@
 package com.learn.services;
 
 import com.learn.dto.request.StudentRegistrationRequestDTO;
+import com.learn.dto.request.StudentUpdateRequestDTO;
 import com.learn.dto.response.StudentResponseDTO;
 import com.learn.dto.response.StudentSummaryDTO;
 
 import java.util.List;
 
 public interface StudentService {
-
+    //Important CRUD Methods
     StudentResponseDTO registerStudent(StudentRegistrationRequestDTO dto);
     StudentResponseDTO getStudentById(Long id);
     List<StudentSummaryDTO> getAllStudents();
+    StudentResponseDTO  updateStudent(Long id , StudentUpdateRequestDTO request);
+    StudentResponseDTO  updateStudentgpa(Long id , StudentUpdateRequestDTO request);
+    void deleteStudent(Long id);
 }
