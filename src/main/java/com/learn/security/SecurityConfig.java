8ce17b699227a22baaf9d61eb370b1df17c78594
Suckscorpio999 <olinabin214@gmail.com>
2026-03-12 20:18:03 +0545

    feat: add SecurityConfig with endpoint protection

--- src/main/java/com/learn/security/SecurityConfig.java
+++ src/main/java/com/learn/security/SecurityConfig.java
@@ -18,7 +18,6 @@ public class SecurityConfig {
                 .csrf(csrf->csrf.disable())
                 .authorizeHttpRequests(auth->auth
                         .requestMatchers("/api/students/register").permitAll()
-                        .requestMatchers("/api/students").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(httpBasic-> {});
