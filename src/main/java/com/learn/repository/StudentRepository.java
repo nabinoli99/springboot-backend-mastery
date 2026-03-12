ff5d94beec41b0e64b4af43d92a94c5381a816b9
Suckscorpio999 <olinabin214@gmail.com>
2026-03-12 20:16:52 +0545

    feat: add findByEmail method in StudentRepository

--- src/main/java/com/learn/repository/StudentRepository.java
+++ src/main/java/com/learn/repository/StudentRepository.java
@@ -4,8 +4,10 @@ import com.learn.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long>
 {
-    Student findByEmail(String email);
+    Optional<Student> findByEmail(String email);
 }
