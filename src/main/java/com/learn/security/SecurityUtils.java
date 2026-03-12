be52b4c69f5170c014e6cadb8f29f70919f77db2
Suckscorpio999 <olinabin214@gmail.com>
2026-03-12 20:16:35 +0545

    feat: add SecurityUtils to get current logged-in user

---
+++ src/main/java/com/learn/security/SecurityUtils.java
@@ -0,0 +1,23 @@
+package com.learn.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class SecurityUtils {
+
+    public static String getCurrentUserEmail(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication == null || authentication.isAuthenticated()){
+            return null;
+        }
+        Object principal = authentication.getPrincipal();
+
+        if(principal instanceof UserDetails userDetails){
+            return userDetails.getUsername();
+        }
+
+        return principal.toString();
+    }
+}
