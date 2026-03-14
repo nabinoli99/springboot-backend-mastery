5d2b839c849ccb49d2272575d94ee54021ff16e6
Suckscorpio999 <olinabin214@gmail.com>
2026-03-14 11:37:23 +0545

    feat: update getAllStudents method to support pagination and sorting

--- src/main/java/com/learn/services/StudentService.java
+++ src/main/java/com/learn/services/StudentService.java
@@ -1,5 +1,6 @@
 package com.learn.services;
 
+import com.learn.common.PageResponse;
 import com.learn.dto.request.StudentRegistrationRequestDTO;
 import com.learn.dto.request.StudentUpdateRequestDTO;
 import com.learn.dto.response.StudentResponseDTO;
@@ -11,8 +12,8 @@ public interface StudentService {
     //Important CRUD Methods
     StudentResponseDTO registerStudent(StudentRegistrationRequestDTO dto);
     StudentResponseDTO getStudentById(Long id);
-    List<StudentSummaryDTO> getAllStudents();
     StudentResponseDTO  updateStudent(Long id , StudentUpdateRequestDTO request);
     StudentResponseDTO  updateStudentgpa(Long id , StudentUpdateRequestDTO request);
     void deleteStudent(Long id);
+    PageResponse<StudentSummaryDTO> getAllStudents(int page, int size , String sortBy , String sortDir);
 }
